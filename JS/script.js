278df4fecfd79f6edf825ef2c5a8d054716d7b09
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-12-03 12:23:51 +0400

    update

---
+++ JS/script.js
@@ -0,0 +1,43 @@
+const icons = document.querySelectorAll('.nav-right i');
+
+icons.forEach(icon => {
+    icon.addEventListener('click', () => {
+
+        // Remove active from all icons
+        icons.forEach(i => i.classList.remove('active'));
+
+        // Add active to clicked icon
+        icon.classList.add('active');
+    });
+});
+
+const userIcon = document.querySelector(".user-icon");
+const userMenu = document.querySelector(".user-menu");
+
+if (userIcon) {
+    userIcon.addEventListener("click", (e) => {
+        e.stopPropagation();
+        userMenu.classList.toggle("show");
+    });
+}
+
+document.addEventListener("click", () => {
+    if (userMenu) userMenu.classList.remove("show");
+});
+
+
+
+
+const menuBtn = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.style.display =
+        navLinks.style.display === "flex" ? "none" : "flex";
+
+    navLinks.style.flexDirection = "column";
+    navLinks.style.background = "#f1f4f2";
+    navLinks.style.padding = "15px";
+    navLinks.style.borderRadius = "10px";
+});
+
